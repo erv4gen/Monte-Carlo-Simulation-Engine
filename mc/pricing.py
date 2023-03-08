69b27b56428139bf853e33fc456e1c534e3b9145
Vladimir Eremin <eremin.vladimir.d@gmail.com>
2023-03-07 20:20:12 -0500

    added bsm prices

--- mc/pricing.py
+++ mc/pricing.py
@@ -39,6 +39,44 @@ def create_option(spot_price:float,strike:float, maturity:int, volatility:float,
         return option
         
 
+
+
+def create_option(spot_price:float,strike:float, maturity:int, volatility:float,risk_free_rate:float, dividend_rate:float, option_type:OptionType) -> ql.VanillaOption:
+        option_type_ql = ql.Option.Call if option_type == OptionType.CALL else ql.Option.Put
+        # Get the current date
+        init_date = ql.Date.todaysDate()
+        # Set the evaluation date to eval_date
+        ql.Settings.instance().evaluationDate = init_date
+        
+        # Set up the option parameters
+        payoff = ql.PlainVanillaPayoff(option_type_ql, strike)
+        exercise = ql.EuropeanExercise(init_date + ql.Period(maturity, ql.Days))
+
+        option = ql.VanillaOption(payoff, exercise)
+        
+        # Set up the market data
+        spot_price = ql.SimpleQuote(spot_price) 
+        risk_free_rate = ql.SimpleQuote(risk_free_rate) 
+        sigma = ql.SimpleQuote(volatility)
+        dividend_rate= ql.SimpleQuote(dividend_rate)
+
+        riskFreeCurve = ql.FlatForward(0, ql.TARGET(), ql.QuoteHandle(risk_free_rate), ql.Actual360())
+        dividend_yield = ql.FlatForward(0, ql.TARGET(), ql.QuoteHandle(dividend_rate), ql.Actual360())
+        volatility = ql.BlackConstantVol(0, ql.TARGET(), ql.QuoteHandle(sigma), ql.Actual360())
+        
+
+        process = ql.BlackScholesMertonProcess(ql.QuoteHandle(spot_price)
+                                            ,ql.YieldTermStructureHandle(dividend_yield)
+                                            ,ql.YieldTermStructureHandle(riskFreeCurve)
+                                            ,ql.BlackVolTermStructureHandle(volatility)
+                                            )
+        
+        # Calculate the option price
+        engine = ql.AnalyticEuropeanEngine(process)
+        option.setPricingEngine(engine)
+        return option
+        
+
 class QlEuropeanOption:
     def __init__(self,spot_price:float,strike:float, maturity:int, volatility:float,risk_free_rate:float, dividend_rate:float, option_type:OptionType) -> None:
         option_type_ql = ql.Option.Call if option_type == OptionType.CALL else ql.Option.Put
