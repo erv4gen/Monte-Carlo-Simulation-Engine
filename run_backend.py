96e05ec753bfcb076e1106a121b3f772d273dde7
Vladimir Eremin <eremin.vladimir.d@gmail.com>
2024-01-23 08:10:20 -0500

    disabled API

--- run_backend.py
+++ run_backend.py
@@ -24,16 +24,16 @@ def run_flask(port):
 if __name__ == "__main__":
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        flask_port = parse_args_port()
+        # flask_port = parse_args_port()
         
-        flask_process = Process(target=run_flask,args=(flask_port,))
+        # flask_process = Process(target=run_flask,args=(flask_port,))
 
         try:
             # Starting the processes
-            flask_process.start()
+            # flask_process.start()
             run_gradio()
         except Exception as e:
             print(e)
-        finally:            
+        # finally:            
             # gradio_process.join()
-            flask_process.join()
+            # flask_process.join()
